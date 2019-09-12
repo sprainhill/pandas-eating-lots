b3da33713d55d8dc3b640c524f4c155cb087ea9e
unknown <rory.christian.murray@gmail.com>
2019-09-12 10:03:30 -0400

    Test commit from another machine

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
+// test
+
 export default ({ data }) => {
   console.log(data)
   return (
