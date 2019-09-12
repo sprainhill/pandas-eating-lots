f4c345cbf6d2ddae6dcef4a36ddbc40783d6aa5e
Rory Murray <rory.christian.murray@gmail.com>
2019-09-12 08:41:00 -0400

    gatsby-node changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,10 @@
-exports.onCreateNode = ({ node }) => {
-  console.log("gatsby-node node.internal.type", node.internal.type)
+exports.onCreateNode = ({ node, getNode }) => {
+  //   console.log("gatsby-node node.internal.type", node.internal.type)
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
+  }
 }
 
 // The onCreateNode function is called by Gatsby whenever a new node is created or updated
