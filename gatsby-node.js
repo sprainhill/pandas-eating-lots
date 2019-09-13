285df18af9d5189c0de69975b19db72b68e648bc
Rory Murray <rory.christian.murray@gmail.com>
2019-09-13 11:41:46 -0400

    Fix file structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,10 +46,10 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`.src/templates/blog-post.js`),
+      component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         // Data passed to context is available
-        // in page queries as GraphQL variables
+        // in page queries as GraphQL variables.
         slug: node.fields.slug,
       },
     })
