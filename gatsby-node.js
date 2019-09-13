d1749abd49e3f95ca65e533ffbbb955ec33bb2c9
Rory Murray <rory.christian.murray@gmail.com>
2019-09-13 11:33:04 -0400

    Update gatsby-node with the blog post template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,13 @@
+const path = require(`path`)
+
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   //   console.log("gatsby-node node.internal.type", node.internal.type)
-  
+
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-
     const slug = createFilePath({ node, getNode, basePath: `pages` })
 
     createNodeField({
@@ -15,18 +16,18 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug,
     })
 
-
     console.log(createFilePath({ node, getNode, basePath: `pages` }))
   }
 }
 
-// The onCreateNode function is called by Gatsby whenever a 
+// The onCreateNode function is called by Gatsby whenever a
 // new node is created or updated
-
 
 exports.createPages = async ({ graphql, actions }) => {
   // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+
+  const { createPage } = actions
+
   const result = await graphql(`
     query {
       allMarkdownRemark {
@@ -41,6 +42,18 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
   console.log(JSON.stringify(result, null, 4))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`.src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
 
 // Adds an implementation of the createPages API which Gatsby
